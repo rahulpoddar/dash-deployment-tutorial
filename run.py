ec8a8e21c3606ca6d1c25225122a2c0e6b402f5e
rahulpoddar <rahul.pod@gmail.com>
2020-04-13 13:32:07 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import dash
-import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import pandas as pd
+import dash_table
+from dash.dependencies import Input, Output, State
 from flask import Flask
 import os
 
@@ -10,33 +12,66 @@ server.secret_key = os.environ.get('secret_key', 'secret')
 app = dash.Dash(name = __name__, server = server)
 #app.config.supress_callback_exceptions = True
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-    dcc.Graph(
-        id='example-graph',    
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    ),
-    dcc.Input(id='my-id', value='initial value', type="text"),
-    html.Div(id='my-div')
+df = pd.read_excel('./TASK1_annotated_1.xlsx', sheet_name = 'Newone')
+
+tasks = df['Kaggle Task name'].unique().tolist()
+
+def generate_summary(task):
+    return 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+def generate_table(dff):
+    rel_cols = ['Document id_', 'Output']
+    return dash_table.DataTable(
+            id = 'search-results-table',
+            columns = [{"name": i, "id": i} for i in dff[rel_cols].columns],
+            data = dff[rel_cols].to_dict('records'),
+            style_data={
+        'whiteSpace': 'normal',
+        'height': 'auto'
+    },
+        style_cell={'textAlign': 'left'},
+        style_header={
+        'backgroundColor': 'rgb(230, 230, 230)',
+        'fontWeight': 'bold'
+    },
+            )
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app.layout = html.Div([
+        html.Div([
+        html.H1('COVID-19 Open Research Dataset Challenge (CORD-19)'),
+        dcc.Dropdown(
+        id='task-dropdown',
+        options=[
+            {'label': i, 'value': i} for i in tasks 
+        ],
+        placeholder="Select a task",
+    )]),
+    
+    html.Div([html.H3('Response Summary', id = 'task-summary-heading'),
+    html.Div(id = 'task-summary')]),
+    
+    html.Div([
+            html.H3('Search Results'),
+            html.Div(id = 'search-results')
+            ])
 ])
 
+
 @app.callback(
-    Output(component_id='my-div', component_property='children'),
-    [Input(component_id='my-id', component_property='value')]
-)
-def update_output_div(input_value):
-    return 'You\'ve entered "{}"'.format(input_value)
+    dash.dependencies.Output('task-summary', 'children'),
+    [dash.dependencies.Input('task-dropdown', 'value')])
+def update_summary(value):
+    return generate_summary(value)
+
+@app.callback(
+    dash.dependencies.Output('search-results', 'children'),
+    [dash.dependencies.Input('task-dropdown', 'value')])
+def update_search_results(value):
+    dff = df[df['Kaggle Task name'] == value]
+    return generate_table(dff)
+    
 
 if __name__ == '__main__':
     app.run_server(debug=True)
